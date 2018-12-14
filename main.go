13b9c39e172bfeff04e7116716b8da93240f0be0
zeeraw <zee@vall.in>
2018-12-14 12:18:00 +0000

    reafctor: move the cli into its own package

--- main.go
+++ main.go
@@ -1,94 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"io/ioutil"
-	"log"
 	"os"
 
-	"github.com/zeeraw/protogen/config"
-	"github.com/zeeraw/protogen/generator"
-
-	"github.com/urfave/cli"
+	"github.com/zeeraw/protogen/cli"
 )
-
-const (
-	// VERSION is the package version
-	VERSION = "1.0.0"
-
-	// Name is the name of the command line tool
-	Name = "protogen"
-
-	// Usage is the description of the tool displayed in the cli help section
-	Usage = "Helps generate and organise your code from .proto files"
-
-	loggerTag = "protogen "
-)
-
-var (
-	authorPhilipV = cli.Author{
-		Name:  "Philip V. (Zee)",
-		Email: "zee@vall.in",
-	}
-)
-
-// Runner holds the configuration for the running context
-type Runner struct {
-	configFilePath string
-	verbose        bool
-}
-
-var runner *Runner
 
 func main() {
-	runner = &Runner{}
-	app := cli.NewApp()
-	app.Name = Name
-	app.Usage = Usage
-	app.Version = VERSION
-	app.Authors = []cli.Author{authorPhilipV}
-	app.Action = action
-	app.Flags = []cli.Flag{
-
-		cli.StringFlag{
-			Name:        "protogen_file",
-			Usage:       "path to the protogen configuration file for the current project",
-			Destination: &runner.configFilePath,
-			Value:       "./.protogen",
-			EnvVar:      "PROTOGEN_FILE",
-		},
-		cli.BoolFlag{
-			Name:        "verbose",
-			Usage:       "use this to show more information",
-			Destination: &runner.verbose,
-		},
-	}
-	app.Run(os.Args)
-}
-
-func action(cc *cli.Context) error {
-	l := logger()
-	cfg, err := ReadConfigFromFilePath(l, runner.configFilePath)
-	if err != nil {
-		return err
-	}
-	cfg.General = &config.General{
-		Verbose: runner.verbose,
-	}
-	if err := generator.Generate(l, cfg); err != nil {
-		fmt.Println(err)
-		return err
-	}
-	return nil
-}
-
-func logger() *log.Logger {
-	var logdest io.Writer
-	if runner.verbose {
-		logdest = os.Stdout
-	} else {
-		logdest = ioutil.Discard
-	}
-	return log.New(logdest, loggerTag, log.Lshortfile|log.LstdFlags)
+	cli.Run(os.Args)
 }
