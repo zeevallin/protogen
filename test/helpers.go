a16788f335dd4bcef2315f6e85e069e9dfd2adf0
zeeraw <zee@vall.in>
2018-12-17 13:36:08 +0100

    test: print the actual value of the assert

--- test/helpers.go
+++ test/helpers.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -8,6 +9,7 @@ import (
 func AssertEqual(tb testing.TB, expected, actual interface{}) {
 	tb.Helper()
 	if expected != actual {
-		tb.Fatalf("\n\texp: %#[1]v (%[1]T)\n\tgot: %#[2]v (%[2]T)\n", expected, actual)
+		fmt.Println(actual)
+		tb.Fatalf("\n\texp: %#[1]v (%[1]T)\n\tgot: %#[2]v (%[2]T)\n\n\t%s", expected, actual, actual)
 	}
 }
