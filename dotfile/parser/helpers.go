3f2dd0ae5188a954a039adc954fdd71d2c20374d
zeeraw <zee@vall.in>
2018-12-18 14:33:29 +0100

    refactor: move helpers into helpers file

---
+++ dotfile/parser/helpers.go
@@ -0,0 +1,82 @@
+package parser
+
+import "github.com/zeeraw/protogen/dotfile/token"
+
+func (p *Parser) skipWhitespaceUntilAny(ts ...token.Type) bool {
+	if !p.expectPeek(token.WHITESPACE) {
+		return false
+	}
+	for p.curTokenIs(token.WHITESPACE) {
+		p.Next()
+	}
+	if !p.expectAny(ts...) {
+		return false
+	}
+	return true
+}
+
+func (p *Parser) skipWhitespaceUntil(t token.Type) bool {
+	if !p.expectPeek(token.WHITESPACE) {
+		return false
+	}
+	for p.curTokenIs(token.WHITESPACE) {
+		p.Next()
+	}
+	if !p.expect(t) {
+		return false
+	}
+	return true
+}
+
+func (p *Parser) curTokenIs(t token.Type) bool {
+	return p.Token().Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.Type) bool {
+	return p.Peek().Type == t
+}
+
+func (p *Parser) expectAny(ts ...token.Type) bool {
+	for _, t := range ts {
+		if p.curTokenIs(t) {
+			return true
+		}
+	}
+	p.Next()
+	return false
+}
+
+func (p *Parser) expect(t token.Type) bool {
+	if p.curTokenIs(t) {
+		return true
+	}
+	p.Next()
+	return false
+}
+
+func (p *Parser) expectAnyPeek(ts ...token.Type) bool {
+	for _, t := range ts {
+		if p.peekTokenIs(t) {
+			p.Next()
+			return true
+		}
+	}
+	return false
+}
+
+func (p *Parser) expectPeek(t token.Type) bool {
+	if p.peekTokenIs(t) {
+		p.Next()
+		return true
+	}
+	p.peekError(t)
+	return false
+}
+
+func (p *Parser) isTerminus() bool {
+	return p.curTokenIs(token.NEWLINE) || p.curTokenIs(token.EOF)
+}
+
+func (p *Parser) isBlockStart() bool {
+	return p.curTokenIs(token.LEFTBRACE)
+}
