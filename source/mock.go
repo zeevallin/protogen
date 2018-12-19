bce826d4c088fcd7c46b84dc710bb55e9f4863af
zeeraw <zee@vall.in>
2018-12-19 15:07:20 +0100

    doc: explain what the methods on the mock source does

--- source/mock.go
+++ source/mock.go
@@ -17,22 +17,27 @@ func NewMockSource(logger *log.Logger, p string) (*MockSource, error) {
 	}, nil
 }
 
+// Init is a no-op
 func (s *MockSource) Init() error {
 	return nil
 }
 
+// PathTo returns the path of the mock fixture package
 func (s *MockSource) PathTo(pkg string) string {
 	return path.Join(s.RootPath(), pkg)
 }
 
+// RootPath returns the root path of the mock fixtures
 func (s *MockSource) RootPath() string {
 	return s.repoPath
 }
 
+// Checkout is a no-op
 func (s *MockSource) Checkout(hash string) error {
 	return nil
 }
 
+// HashForRef is a no-op that returns an empty string
 func (s *MockSource) HashForRef(ref Ref) (string, error) {
 	return "", nil
 }
