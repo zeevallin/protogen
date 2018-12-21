643c5053d6bcace892747f662548b4be58a8a56b
zeeraw <zee@vall.in>
2018-12-21 15:02:02 +0100

    feat: add swift config reference

--- config/languages.go
+++ config/languages.go
@@ -2,19 +2,28 @@ package config
 
 import (
 	"github.com/zeeraw/protogen/config/go"
+	"github.com/zeeraw/protogen/config/swift"
 )
 
 // Language represents programming languages to generate code for
 type Language string
 
 // Languages is a list of all available languages
-var Languages = []Language{Go}
+var Languages = []Language{
+	Go,
+	Swift,
+}
 
 const (
 	// Go represents the programming language Go
 	// https://golang.org/
 	// https://github.com/golang/protobuf
 	Go = Language("go")
+
+	// Swift represents the programming language Swift
+	// https://swift.org/
+	// https://github.com/apple/swift-protobuf
+	Swift = Language("swift")
 )
 
 // ForLanguage returns the appropriate configuration for a given programming language
@@ -26,6 +35,8 @@ func ForLanguage(lang Language) interface{} {
 				golang.GRPC,
 			},
 		}
+	case Swift:
+		return swift.Config{}
 	}
 	panic("cannot find language")
 }
---
+++ config/swift/swift.go
@@ -0,0 +1,4 @@
+package swift
+
+// Config is configuration specific for the programming language Swift
+type Config struct{}
